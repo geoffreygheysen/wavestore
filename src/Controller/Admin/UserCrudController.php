ed0bddfcd10dd2bcdd2c95d4c26a273291f09b24
geoffrey gheysen <geoffrey.gheysen@hotmail.com>
2020-09-07 15:10:08 +0200

    generate HomeController + fix registration form

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,6 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class UserCrudController extends AbstractCrudController
@@ -12,14 +16,20 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('firstname'),
+            TextField::new('lastname'),
+            EmailField::new('email'),
+            ChoiceField::new('sexe')
+                ->setChoices([
+                    '' => '',
+                    'homme' => 'homme',
+                    'femme' => 'femme',
+                    ]),
+            DateField::new('date_of_birth'),
         ];
     }
-    */
+
 }
--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -5,9 +5,14 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegistrationType extends AbstractType
@@ -15,19 +20,55 @@ class RegistrationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('password', PasswordType::class)
-            ->add('confirm_password', PasswordType::class)
-            ->add('firstname')
-            ->add('lastname')
-            ->add('date_of_birth', DateType::class)
+            ->add('email', EmailType::class, [
+                'label' => "Email",
+                'attr' => [
+                    'placeholder' => "John.doe@mail.com",
+                ],
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => "Mot de passe",
+            ])
+            ->add('confirm_password', PasswordType::class, [
+                'label' => "Confirmer le mot de passe",
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => "Prénom",
+                'attr' => [
+                    'placeholder' => "John",
+                ],
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => "Nom",
+                'attr' => [
+                    'placeholder' => "Doe",
+                ],
+            ])
+            ->add('date_of_birth', BirthdayType::class, [
+                'label' => "Date de naissance",
+            ])
             ->add('sexe', ChoiceType::class, [
+                'label' => "Sexe",
                 'choices' => [
                     '' => null,
                     'Homme' => 'homme',
                     'Femme' => 'femme',
                 ],
-            ]);
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ])
+            ->add('btSignin',SubmitType::class, [
+                'label' => "S'inscrire",
+                'attr' => [
+                    'class' => 'btn btn-primary'
+                ],
+            ])
         ;
     }
 
